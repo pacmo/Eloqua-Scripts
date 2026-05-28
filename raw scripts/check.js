fd22941a56679cb513ff56c0bb3edbbbd2530679
pacmo <dmoses@paciolan.com>
2026-05-28 09:14:40 -0700

    Clean seats fix , check.js

---
+++ raw scripts/check.js
@@ -0,0 +1,6 @@
+// check v.0.1 31JAN23
+// verifies if checkbox is checked
+function check(cb) {
+  let ischecked = document.getElementById(cb).checked;
+  return ischecked;
+}
--- raw scripts/cleanSeats.js
+++ raw scripts/cleanSeats.js
@@ -1,47 +1,34 @@
-// cleanSeats v0.3 15FEB24
+// cleanSeats v0.12 20OCT22
 function cleanSeats(colNumber) {
   var tableElem = window.document.getElementById("tickets");
   var tableBody = tableElem.getElementsByTagName("tbody").item(0);
   var i;
   var totalRows = tableBody.rows.length;
-  for (
-    i = 0;
-    i < totalRows;
-    i++ // skip first and last row (hence i=1, and howManyRows-1)
-  ) {
+  for (i = 0; i < (totalRows); i++) // skip first and last row (hence i=1, and howManyRows-1)
+  {
     var thisTrElem = tableBody.rows[i];
     // user entry to javascript leading 0
     var thisTdElem = thisTrElem.cells[colNumber - 1];
     var thisTextNode = thisTdElem.textContent;
-    // 3rd check is for PMAPP blank space
-    if (
-      thisTextNode != "undefined" &&
-      thisTextNode != "" &&
-      thisTextNode.replace(/\s/g, "").length
-    ) {
-      var seatArray = thisTextNode.split(" ");
-      var showCleanSeats = "";
-      for (var s in seatArray) {
-        var cleanSeat = seatArray[s].split(":");
-        cleanSeat[1] = "Sec: " + cleanSeat[1];
-        cleanSeat[2] = " Row: " + cleanSeat[2];
-        let spacer;
-        if (cleanSeat[3]) {
-          spacer = cleanSeat[3].trim();
-
-          let singleSeatCheck = spacer.split(",");
-          if (singleSeatCheck[0] === singleSeatCheck[1]) {
-            cleanSeat[3] = singleSeatCheck[0];
-          } else {
-            cleanSeat[3] = cleanSeat[3].replace(/,/g, "-");
-          }
-          cleanSeat[3] = " Seat(s): " + cleanSeat[3];
-          cleanSeat.shift();
-          seatArray[s] = cleanSeat.join(":");
-          showCleanSeats += seatArray[s] + "<br>";
-          thisTdElem.innerHTML = showCleanSeats;
-        }
-      } // end for
-    } // end if
-  }
-}
+    var seatArray = thisTextNode.split(" ");
+    console.log(seatArray);
+    var showCleanSeats = "";
+    for (var s in seatArray) {
+      var cleanSeat = seatArray[s].split(":");
+      cleanSeat[1] = "Sec: " + cleanSeat[1];
+      cleanSeat[2] = " Row: " + cleanSeat[2];
+      var spacer = cleanSeat[3].trim();
+      var singleSeatCheck = spacer.split(",");
+      if (singleSeatCheck[0] === singleSeatCheck[1]) {
+        cleanSeat[3] = singleSeatCheck[0];
+      } else {
+        cleanSeat[3] = cleanSeat[3].replace(/,/g, '-');
+      }
+      cleanSeat[3] = " Seat(s): " + cleanSeat[3];
+      cleanSeat.shift();
+      seatArray[s] = cleanSeat.join(":");
+      showCleanSeats += seatArray[s] + "<br>";
+    }
+    thisTdElem.innerHTML = showCleanSeats;
+  } // end for
+};
