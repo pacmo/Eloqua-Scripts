e8ce66565cb8ef1b7dd515fb03f0732cb08b663c
pacmo <dmoses@paciolan.com>
2026-06-04 13:23:59 -0700

    setProcFee v0.2

--- raw scripts/setProcFee.js
+++ raw scripts/setProcFee.js
@@ -17,3 +17,23 @@ function setProcFee() {
   console.log("tixTot: " + tixTot);
   console.log("procFee: " + procFee);
 }
+// setProcFee v.0.2 15JUN23
+// requires inRange function, procFeeArray
+function setProcFee(array) {
+  let pFee = 0;
+  array.forEach((item) => {
+    if (typeof item.max === "undefined") {
+      item.max = 999999999;
+    }
+    let tixTot = window.document.getElementById("tickets-total").innerText;
+    tixTot = tixTot.replace(/<\/?span[^>]*>/g, "");
+    tixTot = Number(tixTot.replace(/[^0-9.-]+/g, ""));
+    if (inRange(tixTot, item.min, item.max)) {
+      pFee = item.fee;
+    }
+  });
+  pFee =
+    "$" +
+    new Intl.NumberFormat("en-US", { minimumFractionDigits: 2 }).format(pFee);
+  document.getElementById("proc-fee").innerHTML = pFee;
+}
