44c0baf6301a8ef5b9894a3d52768becc03760e5
pacmo <dmoses@paciolan.com>
2026-06-09 11:37:36 -0700

    startup/cleanseats

--- raw scripts/cleanSeats.js
+++ raw scripts/cleanSeats.js
@@ -1,38 +1,46 @@
-// cleanSeats v0.13 14NOV22
+// cleanSeats v0.13.1 CUSTOM 17MAY23
 function cleanSeats(colNumber) {
-  var tableElem = window.document.getElementById("tickets");
-  var tableBody = tableElem.getElementsByTagName("tbody").item(0);
-  var i;
-  var totalRows = tableBody.rows.length;
-  for (
-    i = 0;
-    i < totalRows;
-    i++ // skip first and last row (hence i=1, and howManyRows-1)
-  ) {
-    var thisTrElem = tableBody.rows[i];
-    // user entry to javascript leading 0
-    var thisTdElem = thisTrElem.cells[colNumber - 1];
-    var thisTextNode = thisTdElem.textContent;
-    if (thisTextNode != "undefined" && thisTextNode != "") {
-      var seatArray = thisTextNode.split(" ");
-      var showCleanSeats = "";
+  var tableElem,
+    tableBody = window.document
+      .getElementById("tickets")
+      .getElementsByTagName("tbody")
+      .item(0),
+    i,
+    totalRows = tableBody.rows.length;
+  for (i = 0; i < totalRows; i++) {
+    var thisTrElem,
+      thisTdElem = tableBody.rows[i].cells[colNumber - 1],
+      thisTextNode = thisTdElem.textContent,
+      // last check is for whitespace only - required for PM App
+      thisDescElem = tableBody.rows[i].cells[0],
+      descNode = thisDescElem.textContent;
+    if (
+      typeof thisTextNode !== "undefined" &&
+      thisTextNode !== "" &&
+      thisTextNode.replace(/\s/g, "").length
+    ) {
+      var seatArray = thisTextNode.split(" "),
+        showCleanSeats = "";
       for (var s in seatArray) {
         var cleanSeat = seatArray[s].split(":");
-        cleanSeat[1] = "Sec: " + cleanSeat[1];
-        cleanSeat[2] = " Row: " + cleanSeat[2];
-        var spacer = cleanSeat[3].trim();
-        var singleSeatCheck = spacer.split(",");
-        if (singleSeatCheck[0] === singleSeatCheck[1]) {
-          cleanSeat[3] = singleSeatCheck[0];
+        // update PARKING
+        if (descNode.indexOf("Parking") > -1) {
+          thisTdElem.innerHTML = "LOT: " + cleanSeat[1];
         } else {
-          cleanSeat[3] = cleanSeat[3].replace(/,/g, "-");
+          (cleanSeat[1] = "Sec: " + cleanSeat[1]),
+            (cleanSeat[2] = " Row: " + cleanSeat[2]);
+          var spacer,
+            singleSeatCheck = cleanSeat[3].trim().split(",");
+          singleSeatCheck[0] === singleSeatCheck[1]
+            ? (cleanSeat[3] = singleSeatCheck[0])
+            : (cleanSeat[3] = cleanSeat[3].replace(/,/g, "-")),
+            (cleanSeat[3] = " Seat(s): " + cleanSeat[3]),
+            cleanSeat.shift(),
+            (seatArray[s] = cleanSeat.join(":")),
+            (showCleanSeats += seatArray[s] + "<br>"),
+            (thisTdElem.innerHTML = showCleanSeats);
         }
-        cleanSeat[3] = " Seat(s): " + cleanSeat[3];
-        cleanSeat.shift();
-        seatArray[s] = cleanSeat.join(":");
-        showCleanSeats += seatArray[s] + "<br>";
-        thisTdElem.innerHTML = showCleanSeats;
-      } // end for
-    } // end if
+      }
+    }
   }
 }
